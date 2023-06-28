63bfb2c630acc2dbf1f7f70e176580f55428a30d
LucasMatusalem <lucas@matusa.dev>
2023-06-28 12:53:06 -0300

    change dockerfile and add route health check

--- src/interfaces/http/routes.ts
+++ src/interfaces/http/routes.ts
@@ -3,7 +3,9 @@ import { collectionsRoutes } from "./collections/collections.routes";
 
 const router = Router();
 router.use('/collections', collectionsRoutes);
-
+router.use('/', (_req, res) => {
+  res.send('OK')
+})
 export {
   router
 }
