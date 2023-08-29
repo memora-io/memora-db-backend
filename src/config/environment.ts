d114d9cfbd857dfba29bff36dbedbc5e265c0551
LucasMatusalem <lucas@matusa.dev>
2023-08-29 15:54:04 -0300

    chore: add new environments to validator and get then from environemnt @config/

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -3,9 +3,14 @@ import { z } from "zod";
 const envSchema = z.object({
   QDRANT_API_URL: z.coerce.string().nonempty(),
   DATABASE_URL: z.coerce.string().nonempty(),
+  STRIPE_KEY: z.coerce.string().nonempty(),
+  MIXPANEL_TOKEN: z.coerce.string().nonempty(),
+  CLERK_SECRET_KEY: z.coerce.string().nonempty(),
 });
 
+
 const environment = envSchema.parse(process.env)
+console.log(`environment: `, environment)
 
 export {
   environment
--- src/utils/get-stripe.ts
+++ src/utils/get-stripe.ts
@@ -1,10 +1,8 @@
+import { environment } from '@/config/environment';
 import Stripe from 'stripe';
 
 export function getStripe(){
-  if(!process.env?.STRIPE_KEY){
-    throw new Error('Can not initialize Stripe without STRIPE_KEY');
-  }
-  const client = new Stripe(process.env.STRIPE_KEY, {
+  const client = new Stripe(environment.STRIPE_KEY, {
       apiVersion: '2023-08-16',
       httpClient: Stripe.createFetchHttpClient(), // use fetch instead of node http
   });
